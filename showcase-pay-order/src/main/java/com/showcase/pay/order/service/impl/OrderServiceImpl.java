a02a49eff054363042d68e1fd8cbebad3f2715b9
yuhata <jdktomcater@gmail.com>
2026-04-12 14:21:28 +0800

    打印日志

--- showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
+++ showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
@@ -99,6 +99,7 @@ public class OrderServiceImpl implements OrderService {
         // Clear cache
         redisTemplate.delete(ORDER_CACHE_PREFIX + orderNo);
         log.info("Order cancelled: orderNo={}", orderNo);
+        log.info("Order cancelled: orderNo={}", orderNo);
     }
 
     @Override
