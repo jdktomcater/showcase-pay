8120edeebbeed938ac58413a224f34e8133bfe18
yuhata <jdktomcater@gmail.com>
2026-04-12 14:23:09 +0800

    打印日志

--- showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
+++ showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
@@ -121,6 +121,7 @@ public class OrderServiceImpl implements OrderService {
         // Clear cache
         redisTemplate.delete(ORDER_CACHE_PREFIX + orderNo);
         log.info("Order payment status updated: orderNo={}, paymentStatus={}", orderNo, paymentStatus);
+        log.info("Order payment status updated: orderNo={}, paymentStatus={}", orderNo, paymentStatus);
     }
 
     private OrderResponse convertToResponse(Order order) {
