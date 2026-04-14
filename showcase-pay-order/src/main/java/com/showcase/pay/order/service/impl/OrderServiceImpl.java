48ddd88390313817f05f54f29624190143dd6603
yuhata <jdktomcater@gmail.com>
2026-04-14 15:16:00 +0800

    日志优化

--- showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
+++ showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
@@ -128,6 +128,7 @@ public class OrderServiceImpl implements OrderService {
         log.info("Order cancelled: orderNo={}", orderNo);
         log.info("Order cancelled: orderNo={}", orderNo);
         log.info("Order cancelled: orderNo={}", orderNo);
+        log.info("Order cancelled: orderNo={}", orderNo);
     }
 
     @Override
