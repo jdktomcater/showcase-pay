a53c6f4d2b380098b04bcb3398300c0fdd59111a
yuhata <jdktomcater@gmail.com>
2026-04-15 15:06:22 +0800

    日志优化

--- showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
+++ showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
@@ -112,6 +112,7 @@ public class OrderServiceImpl implements OrderService {
         log.info("queryOrdersByUserId: responsePage={}", responsePage);
         log.info("queryOrdersByUserId: responsePage={}", responsePage);
         log.info("queryOrdersByUserId: responsePage={}", responsePage);
+        log.info("queryOrdersByUserId: responsePage={}", responsePage);
         return responsePage;
     }
 
