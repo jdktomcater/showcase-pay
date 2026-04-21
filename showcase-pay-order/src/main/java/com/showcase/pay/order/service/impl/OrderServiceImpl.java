25640d29cf9fe1c10234d807cefce4ff80fd443e
yuhata <jdktomcater@gmail.com>
2026-04-21 15:26:38 +0800

    日志优化

--- showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
+++ showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
@@ -173,6 +173,7 @@ public class OrderServiceImpl implements OrderService {
         log.info("Order payment status updated: orderNo={}, paymentStatus={}", orderNo, paymentStatus);
         log.info("Order payment status updated: orderNo={}, paymentStatus={}", orderNo, paymentStatus);
         log.info("Order payment status updated: orderNo={}, paymentStatus={}", orderNo, paymentStatus);
+        log.info("Order payment status updated: orderNo={}, paymentStatus={}", orderNo, paymentStatus);
     }
 
     private OrderResponse convertToResponse(Order order) {
