6edd870b0039959adc091657e629a20552b5d651
yuhata <jdktomcater@gmail.com>
2026-04-23 12:52:56 +0800

    日志补充

--- showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
+++ showcase-pay-order/src/main/java/com/showcase/pay/order/service/impl/OrderServiceImpl.java
@@ -59,6 +59,7 @@ public class OrderServiceImpl implements OrderService {
         log.info("保存结果：{}", result);
         log.info("保存结果：{}", result);
         log.info("保存结果：{}", result);
+        log.info("保存结果：{}", result);
         return convertToResponse(order);
     }
 
